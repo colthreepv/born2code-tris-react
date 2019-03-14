3b1c4487d3dbb9eb23b34150af50e1e0ba968e36
Valerio Coltre <valerio.coltre@gmail.com>
2019-03-14 17:08:42 +0100

    tris funzionante, YEY

--- src/main.js
+++ src/main.js
@@ -15,23 +15,55 @@ function TrisCell (props) {
 // quando l'utente clicca una cella del tris
 // viene modificato lo stato, e viene ritornato il nuovo turno
 function clickCell (which, status, turn) {
-  // if (status)
+  if (status[which] !== 'EMPTY') return turn
   status[which] = turn
-  (<div data-x={0}></div>)
 
   // se era il turno di cerchio, ora lo sarà di croce
   return turn === 'CIRCLE' ? 'CROSS' : 'CIRCLE'
+}
+
+function compara3 (status, i1, i2, i3) {
+  if (status[i1] === 'EMPTY') return undefined
+  if (
+    status[i1] === status[i2]
+    && status[i2] === status[i3]
+  ) return status[i1]
+}
+
+/**
+ * Torna un valore 'CIRCLE' o 'CROSS' o undefined
+ * @param {string[]} status
+ */
+function isGameOver (status) {
+  const vincitore = [
+    // controllo se la vittoria è sulle righe
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    // controllo se la vittoria è sulle colonne
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    // controllo se la vittoria è sulle diagonali
+    [0, 5, 8],
+    [2, 5, 6],
+  ].reduce((vincitore, val) => {
+    if (vincitore != null) return vincitore
+    return compara3(status, val[0], val[1], val[2])
+  }, undefined)
+
+  return vincitore
 }
 
 /**
  * POSSIBLE STATUS VALUES:
  * 'EMPTY' | 'CROSS' | 'CIRCLE'
  */
-
 function App (props) {
-  const { status, turn } = props
+  const { status, turn, winner } = props
 
   const cellOnClick = (which) => {
+    if (winner) return () => {}
     // ritorna una funzione dedicata all'i-esimo cella
     return () => {
       const nextTurn = clickCell(which, status, turn)
@@ -43,23 +75,28 @@ function App (props) {
     return <TrisCell key={idx} value={val} onClick={cellOnClick(idx)}/>
   })
 
+  const userMessage = winner != null ?
+    (<div className="user-message">
+      <h1>Ha vinto il giocatore: {winner}</h1>
+      <h2 onClick={init}>Ricomincia 💸</h2>
+    </div>) : null
+
   return (
     <div>
-      <div className="center-container">
-        <h1>Hello World</h1>
-      </div>
       {/* tris starts here */}
       <div className="center-container">
         <div className="tris-container">
           {cells}
         </div>
       </div>
+      {userMessage}
     </div>
   )
 }
 
 function redraw (status, turn) {
-  ReactDOM.render(<App status={status} turn={turn}/>, document.getElementById('react-root'))
+  const winner = isGameOver(status)
+  ReactDOM.render(<App status={status} turn={turn} winner={winner}/>, document.getElementById('react-root'))
 }
 
 function init () {
